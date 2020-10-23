ee4cc89b03718e518ef525afbda5ab7fe66f65b9
Emma Hack <ehack78@gmail.com>
2020-10-23 15:55:01 +0100

    check song has artist passed

--- test/song_test.py
+++ test/song_test.py
@@ -7,4 +7,7 @@ class TestSong(unittest.TestCase):
         self.example_song_1 = Song("Saturday Night Fever", "The Bee Gees", 3.22)
 
     def test_song_has_a_title(self):
-        self.assertEqual("Saturday Night Fever", self.example_song_1.title)   
+        self.assertEqual("Saturday Night Fever", self.example_song_1.title) 
+
+    def test_song_has_an_artist(self):
+        self.assertEqual("The Bee Gees", self.example_song_1.artist)  
