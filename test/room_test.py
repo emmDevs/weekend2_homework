f71178d15b1c6426878b5175e1a4fb599dafbcc6
Emma Hack <ehack78@gmail.com>
2020-10-23 16:19:52 +0100

    check room has guest name passed

--- test/room_test.py
+++ test/room_test.py
@@ -8,10 +8,13 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.guest = Guest("John Travolta", 34, 50, "Saturday Night Fever")
         self.song = Song("Saturday Night Fever", "The Bee Gees", 3.22)
-        self.example_room_1 = Room("stars in your eyes", 5, Guest, Song)
+        self.example_room_1 = Room("stars in your eyes", 5, self.guest, self.song)
 
     def test_room_has_name(self):
         self.assertEqual("stars in your eyes", self.example_room_1.name)
 
     def test_room_has_a_capacity(self):
         self.assertEqual(5, self.example_room_1.capacity)
+
+    def test_room_has_a_guest(self):
+        self.assertEqual("John Travolta", self.example_room_1.guest.name)
